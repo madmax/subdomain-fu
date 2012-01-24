e1bae15751a9e48dd683a5e77efe2cc055473031
Grzegorz Derebecki <grzegorz.derebecki@fdb.pl>
2012-01-24 12:43:01 +0100

    if constraint contain subdomain set it as options

--- lib/subdomain_fu/url_rewriter.rb
+++ lib/subdomain_fu/url_rewriter.rb
@@ -3,12 +3,25 @@ require 'action_dispatch/routing/route_set'
 module ActionDispatch
   module Routing
     class RouteSet #:nodoc:
+            
+      # this will require class with subdomain and regexp? method
       def url_for_with_subdomains(options, path_segments=nil)
-        if SubdomainFu.needs_rewrite?(options[:subdomain], options[:host]) || options[:only_path] == false
-          options[:only_path] = false if SubdomainFu.override_only_path?
-          options[:host] = SubdomainFu.rewrite_host_for_subdomains(options[:subdomain], options[:host])
+        if options[:use_route] && route = named_routes.get(options[:use_route]) 
+          # get subdomain constraint (need respond to subdomain mathod)     
+          subdomain_constrain = route.app.instance_variable_get("@constraints").to_a.find { |constraint| constraint.respond_to?(:subdomain)}
+          
+          # add subdomain option if subdomain constrain is not regexp
+          options[:subdomain] ||= subdomain_constrain.subdomain.to_s if subdomain_constrain && !subdomain_constrain.regexp?
         end
-        options.delete(:subdomain) if (route_name = options[:use_route]) && (route = named_routes.get(route_name)) && route.requirements[:subdomain].blank?
+        
+        # remove subdomain (it will be rewrited to host)
+        subdomain = options.delete(:subdomain)
+        
+        if SubdomainFu.needs_rewrite?(subdomain, options[:host]) || options[:only_path] == false
+          options[:only_path] = false if SubdomainFu.override_only_path?
+          options[:host] = SubdomainFu.rewrite_host_for_subdomains(subdomain, options[:host])
+        end
+        
         url_for_without_subdomains(options)
       end
       alias_method_chain :url_for, :subdomains
